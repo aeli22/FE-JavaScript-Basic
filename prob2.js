038652cb3bd371a0fdd537b02312b329ceecf635
aeli kim <aelli2002@naver.com>
2024-04-03 13:03:03 +0900

    최종

--- prob2.js
+++ prob2.js
@@ -1,1 +1,13 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(n) {
+    let i = 1;
+    let sum =1;
+    while(true){
+        sum=sum*i;
+         i++; 
+    
+        if(sum>n)
+            return i-2;
+  
+    }
+}
--- prob3.js
+++ prob3.js
@@ -1,1 +1,24 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(arr) {
+    let small= Infinity;
+      let x;
+      
+      if(arr.length!=1){ 
+      for(let i=0;i<arr.length;i++){
+        
+          if(small>arr[i]){
+              small=arr[i]; 
+          x=i;
+          }
+          
+          
+      
+         }
+    arr.splice(x,1);
+  return arr;
+      
+      }
+      else
+          arr=[-1];
+          return arr;
+  }
